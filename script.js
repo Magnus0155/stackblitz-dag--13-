b59778241daf33f08985951030073acac743f68e
Magnus0155 <magnuseeg1@gmail.com>
2024-11-19 12:04:03 +0100

    Det er egentlig dag 14

---
+++ script.js
@@ -0,0 +1,39 @@
+//const redSquare = document.querySelector("#redSquare");
+//console.log(redSquare);
+
+//redSquare.style.height = "100px";
+//redSquare.style.width = "100px";
+//redSquare.style.backgroundColor = "red";//
+
+redSquare.classList.add("redContainer")
+
+//Oppgave 1: Lag en grønn firkant.
+// // Lag en Html og gi den en ID
+// // Hente element til JS
+// // Gi variabel styling properties// 
+
+//const greenSquare = document.querySelector("#greenSquare");
+//console.log(redSquare);
+//greenSquare.style.height = "100px";
+//greenSquare.style.width = "100px";
+//greenSquare.style.backgroundColor = "green";//
+
+greenSquare.classList.add("greenContainer")
+
+
+const cardContainer = document.querySelector
+("#cardContainer");
+console.log(cardContainer);
+
+const cardHeading = document.createElement("h1");
+console.log(cardHeading);
+
+cardHeading.textContent ="God dagen!";
+
+cardContainer.appendChild(cardHeading);
+
+
+// oppgave 2:hent og lag et HTML element
+// // lag et div element i html og gi det en ID
+// // lag en variabel som henter div-element til JS
+// // lag en variabel som lager et h1- element
